5638fdfff56d901116029944342e23a88afca392
Melvin Wijaya <mewijaya@ucsd.edu>
2017-11-28 19:05:41 -0800

    added create recipe

--- recipez-app/src/components/pages/Cookbook.js
+++ recipez-app/src/components/pages/Cookbook.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-import {Tabs, Tab} from 'react-bootstrap';
 import SavedRecipes from "../cookbookComponents/savedRecipes";
 import PersonalRecipes from "../cookbookComponents/personalRecipes";
-import { OffCanvas, OffCanvasMenu, OffCanvasBody } from 'react-offcanvas';
-import { Button, Grid, Row, Col, FormGroup, FormControl, Form, ControlLabel } from 'react-bootstrap';
-import '../../css/Cookbook.css';
+import NewRecipe from "../cookbookComponents/newPersonalRecipe";
+import { Tabs, Tab, FormGroup, FormControl, Form, ControlLabel } from 'react-bootstrap';
 
 
 class Cookbook extends Component {
@@ -13,6 +11,20 @@ class Cookbook extends Component {
         super(props);
 
         this.renderRow = this.renderRow.bind(this);
+        this.state = {
+            showEditor: false
+        };
+
+        this.open = this.open.bind(this);
+        this.close = this.close.bind(this);
+    }
+
+    open() {
+        this.setState( {showEditor: true} );
+    }
+
+    close() {
+        this.setState( {showEditor: false} );
     }
 
     renderRow(){
@@ -21,15 +33,6 @@ class Cookbook extends Component {
 
     }
 
-    componentWillMount() {
-        this.setState({
-            isMenuOpened: false
-        })
-    }
-
-    handleClick(){
-        this.setState({ isMenuOpened: !this.state.isMenuOpened});
-    }
 
 
 
@@ -68,60 +71,7 @@ class Cookbook extends Component {
 
                         {/* TODO: Swing-out personal recipe edit/create area */}
                         <div className="col-md-3">
-                            <OffCanvas className="newRecipe" width='600' transitionDuration='300' isMenuOpened={this.state.isMenuOpened} position="right">
-                                <OffCanvasBody className="navbar-icon">
-                                    <a href="#" className="btn btn-primary" role="button" onClick={this.handleClick.bind(this)}>
-                                        New recipe
-                                    </a>
-                                </OffCanvasBody>
-                                <OffCanvasMenu className="newRecipeMenu">
-                                    {/*Form for the recipe title*/}
-                                        <Form horizontal>
-                                            <FormGroup controlId="formHorizontalTitle">
-                                                Title
-                                                <FormControl type="title" placeholder="Recipe title" />
-                                            </FormGroup>
-                                        </Form>
-
-                                    <div className="row">
-                                        <img src="http://www.vermeer.com.au/wp-content/uploads/2016/12/attachment-no-image-available.png"
-                                             alt="No Image Available"
-                                            height={150}
-                                            width={150} />
-                                    </div>
-
-                                    <div className="row">
-                                        <Button bsStyle="info"
-                                             bsSize="xsmall">
-                                            Upload Image
-                                        </Button>
-                                    </div>
-
-
-                                    {/*Form for the ingredients list*/}
-                                    <div className="row">
-                                            <Form>
-                                                <FormGroup controlId="formControlsTextarea">
-                                                    <ControlLabel>Ingredients:</ControlLabel>
-                                                    <FormControl componentClass="textarea" placeholder ="Enter ingredients list" />
-                                                </FormGroup>
-                                            </Form>
-                                    </div>
-
-                                    {/*Form for the cooking instructions*/}
-                                    <div className="row">
-                                            <Form>
-                                                <FormGroup controlId="formControlsTextarea">
-                                                    <ControlLabel>Instructions:</ControlLabel>
-                                                    <FormControl componentClass="textarea" placeholder ="Enter instructions" />
-                                                </FormGroup>
-                                            </Form>
-                                    </div>
-
-                                    <button type="button" class="btn btn-primary">Create</button>
-                                    <button type="button" class="btn btn-secondary" onClick={this.handleClick.bind(this)}>Close</button>
-                                </OffCanvasMenu>
-                            </OffCanvas>
+                            <NewRecipe />
                         </div>
 
                     </div>
