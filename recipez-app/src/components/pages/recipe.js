889f49d4e43a22a9254a4183b13c0135ab05a1bd
Johnson <jky003@ucsd.edu>
2017-11-28 21:38:38 -0800

    Basic layout 2.0

--- recipez-app/src/components/pages/recipe.js
+++ recipez-app/src/components/pages/recipe.js
@@ -42,12 +42,12 @@ class Recipe extends Component {
         if(!this.state.loaded) {
             return <div><h1>{this.state.data}</h1></div>
         }
-        var ingredients = this.state.data.Ingredients.map((ingredient) => <li><span color={this.state.mousedOver === ingredient ? 'red' : 'black'}>{ingredient}</span></li>)
-        var directions = this.state.data.Directions.map((step) => <li>{step}</li>)
+        var ingredients = this.state.data.Ingredients.map((ingredient) => <li class="list-group-item"><span color={this.state.mousedOver === ingredient ? 'red' : 'black'}>{ingredient}</span></li>)
+        var directions = this.state.data.Directions.map((step) => <li class="list-group-item">{step}</li>)
         var timecost
         var difficulty
         // alert(JSON.stringify(this.state.data))
-        var reviews = Object.entries(this.state.data.Reviews?this.state.data.Reviews:{}).map((review) => 
+        var reviews = Object.entries(this.state.data.Reviews?this.state.data.Reviews:{}).map((review) =>
             <li>
                 <div>
                     <h5 style={{float:'left'}}>{review[1].username}</h5><img src={'/star.jpg'} height='21' width='21'/>
@@ -70,30 +70,29 @@ class Recipe extends Component {
                                     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
                                     <li data-target="#myCarousel" data-slide-to="1"></li>
                                     <li data-target="#myCarousel" data-slide-to="2"></li>
-                                    <li data-target="#myCarousel" data-slide-to="3"></li>
                                 </ol>
 
                                 <div class="carousel-inner">
-                                    <div class="item active">
-                                        <img src="https://images-gmi-pmc.edge-generalmills.com/f48f767c-5e82-4826-a5ca-85e9cfb15920.jpg"/>
+                                    <div class="carousel-item active">
+                                        <img class="d-block img-fluid" src="https://images-gmi-pmc.edge-generalmills.com/f48f767c-5e82-4826-a5ca-85e9cfb15920.jpg" alt="First slide"></img>
                                     </div>
 
-                                    <div class="item">
-                                        <img src="http://images.media-allrecipes.com/userphotos/600x600/4228736.jpg"/>
+                                    <div class="carousel-item">
+                                        <img class="d-block img-fluid" src="http://images.media-allrecipes.com/userphotos/600x600/4228736.jpg" alt="Second slide"></img>
                                     </div>
 
-                                    <div class="item">
-                                        <img src="http://images.media-allrecipes.com/userphotos/600x600/2126904.jpg"/>
+                                    <div class="carousel-item">
+                                        <img class="d-block img-fluid" src="http://images.media-allrecipes.com/userphotos/600x600/2126904.jpg" alt="Third slide"></img>
                                     </div>
                                 </div>
 
                                 {/*<!-- Left and right controls -->*/}
-                                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                                    <span class="glyphicon glyphicon-chevron-left"></span>
+                                <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                     <span class="sr-only">Previous</span>
                                 </a>
-                                <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                                    <span class="glyphicon glyphicon-chevron-right"></span>
+                                <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                     <span class="sr-only">Next</span>
                                 </a>
                             </div>
