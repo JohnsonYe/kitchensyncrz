d74c3938025ddf79ac23d112f315df7b9fbdfaf6
hysang <hysang@ucsd.edu>
2017-12-02 16:35:10 -0800

    Splash page still editing

--- recipez-app/src/components/pages/homePage.js
+++ recipez-app/src/components/pages/homePage.js
@@ -11,11 +11,46 @@ import 'bootstrap/dist/css/bootstrap.css';
 import browseIcon from '../../Image/SplashScreen/Browse-3.jpeg';
 import burgerImage from '../../Image/SplashScreen/burger.jpeg';
 import kitchenImage from '../../Image/SplashScreen/kitchen.jpg';
+import chickenImage from '../../Image/SplashScreen/chicken.jpg';
+import steakImage from '../../Image/SplashScreen/steak.jpg';
+import breadImage from '../../Image/SplashScreen/bread.jpg';
+import appleImage from '../../Image/SplashScreen/apple.jpg';
+import limeImage from '../../Image/SplashScreen/lime.jpg';
+import lemonImage from '../../Image/SplashScreen/lemon.jpg';
+import peachImage from '../../Image/SplashScreen/peach.jpg';
+import tacoImage from '../../Image/SplashScreen/taco.jpg';
 
 const popoverBottom = (
-    <Popover id="popover-positioned-bottom" title="Popover bottom">
-        <strong>Holy guacamole!</strong> Check this info.
+    <Popover id="popover-positioned-bottom" title="About Browse">
+        <strong>Browse</strong> Browse allows you be able to use filter by required
+        ingredients and cookware and/or appliances necessary for particular recipes,
+        as well as time needed, difficulty level and cost to make.
     </Popover>
+
+);
+const popoverBottom1 = (
+    <Popover id="popover-positioned-bottom1" title="About Cookbook">
+        <strong>Cookbook</strong> Cookbook You can resume and edit your recipes whenever you want
+        in <strong>My cookbook page</strong>.
+
+    </Popover>
+
+);
+const popoverBottom2 = (
+    <Popover id="popover-positioned-bottom2" title="About Kitchen">
+        <strong>Kitchen</strong> Kitchen You will able to add items to an exclusion list, according to
+        your dietary preferences in <strong>My Kitchen page</strong>. My Kitchen will also be a list of your
+        own cookware/tools/appliances that you do have access to. Personalizing Kitchen will allow Kitchen Sync
+        to customize each user’s experience according to their situation.
+    </Popover>
+
+);
+
+const popoverBottom3 = (
+    <Popover id="popover-positioned-bottom3" title="About Planner">
+        <strong>Planner</strong> Planner You will able to...."NEED TO ADD Sth HERE"..."EDITING"..
+    </Popover>
+
 );
 
 class Homepage extends Component {
@@ -27,19 +62,20 @@ class Homepage extends Component {
                 <Jumbotron>
                     <div>
                         <h1>Kitchen Sync</h1>
+                        {/*<p className="banner-style"><Image src={bannerImage} responsive /></p>*/}
                     </div>
                 </Jumbotron>
                 <Grid>
                     <Row>
                         <Col sm={5} >
-                            <h1>About Kitchen Sync</h1>
+                            <h1 className="about-style">About Kitchen Sync</h1>
                             <p className="intro-style">
                                 Kitchen Sync is the Web application that aims to be a personalized assistant to help people, who are
                                 traditionally not inclined to cook for themselves or people with little to no experience,
                                 approach this healthy and cost effective practice without any of the usual stress or
                                 problems associated with cooking. And how does it manage to do this? Because, Kitchen
                                 Sync is the cooking app that adapts to the user instead of forcing the user to adapt to it.</p>
-                            <p><a className="btn btn-lg btn-primary" href="#" role="button">Sign up today</a>
+                            <p><a className="btn btn-lg btn-primary" href="/Search" role="button">Sign up today</a>
                             </p>
                         </Col>
                         <Col sm={7}>
@@ -49,23 +85,23 @@ class Homepage extends Component {
                                          src={burgerImage}/>
                                     <Carousel.Caption>
                                         <h3>First Pic</h3>
-                                        <p>FoodPorn1.</p>
+                                        <p>FoodPorn1</p>
                                     </Carousel.Caption>
                                 </Carousel.Item>
                                 <Carousel.Item>
                                     <img className="carousel-image" width={500} height={400} alt="900x500"
-                                         src={burgerImage}/>
+                                         src={steakImage}/>
                                     <Carousel.Caption>
                                         <h3>Second Pic</h3>
-                                        <p>FoodPorn2.</p>
+                                        <p>FoodPorn2</p>
                                     </Carousel.Caption>
                                 </Carousel.Item>
                                 <Carousel.Item>
                                     <img className="carousel-image" width={500} height={400} alt="900x500"
-                                         src={burgerImage}/>
+                                         src={chickenImage}/>
                                     <Carousel.Caption>
                                         <h3>Third Pic</h3>
-                                        <p>FoodPorn3.</p>
+                                        <p>FoodPorn3</p>
                                     </Carousel.Caption>
                                 </Carousel.Item>
                             </Carousel>
@@ -75,16 +111,29 @@ class Homepage extends Component {
                     <Row>
                         <Col sm={3}>
                             <Image src={browseIcon} circle responsive />
-                            <h2>Browse</h2>
+                            <h2 className="feature-style">Browse</h2>
                             <p> Browse will enable the user to browse through our extensive database of recipes
                                 to find something that they wish to attempt. Users will also have an option to
                                 use Kitchen Sync ’s powerful filtering tool to find recipes that specifically
-                                suit their current situation. They will be able to filter by required
-                                ingredients and cookware and/or appliances necessary for particular recipes,
-                                as well as time needed, difficulty level and cost to make.</p>
+                                suit their current situation. </p>
 
                             <ButtonToolbar>
                                 <OverlayTrigger trigger="click" placement="bottom" overlay={popoverBottom}>
+                                    <button className="btn btn-lg btn-primary"><strong>Detail >></strong></button>
+                                </OverlayTrigger>
+                            </ButtonToolbar>
+
+                        </Col>
+
+                        <Col sm={3}>
+                            <Image src={browseIcon} circle responsive />
+                            <h2 className="feature-style">Cookbook</h2>
+                            <p> Cookbook is where you can save your favorite recipes for easy access. It will
+                                also serve as the home for any recipes the user themselves uploaded to Kitchen Sync’s
+                                ever increasing database of wonderful, easy to cook, recipes.</p>
+
+                            <ButtonToolbar>
+                                <OverlayTrigger trigger="click" placement="bottom" overlay={popoverBottom1}>
                                     <button className="btn btn-lg btn-primary">Detail >></button>
                                 </OverlayTrigger>
                             </ButtonToolbar>
@@ -93,34 +142,13 @@ class Homepage extends Component {
 
                         <Col sm={3}>
                             <Image src={browseIcon} circle responsive />
-                            <h2>Cookbook</h2>
-                            <p> Cookbook is where users can save their favorite recipes for easy access. It will
-                                also serve
-                                as the home for any recipes the user themselves uploaded to Kitchen Sync’ s
-                                ever increasing database of wonderful, easy to cook, recipes..</p>
-
-                            <ButtonToolbar>
-                                <OverlayTrigger trigger="click" placement="bottom" overlay={popoverBottom}>
-                                    <button className="btn btn-lg btn-primary">Detail >></button>
-                                </OverlayTrigger>
-                            </ButtonToolbar>
-
-                        </Col>
-
-                        <Col sm={3}>
-                            <Image src={browseIcon} circle responsive />
-                            <h2>Kitchen</h2>
+                            <h2 className="feature-style">Kitchen</h2>
                             <p> Kitchen is where users can personalize the app and set their preferences. They
-                                can add and remove items in their Pantry , which will tell Kitchen Sync what
-                                ingredients they currently have. Users will also be able to add items to an
-                                exclusion
-                                list, according to their dietary preferences. There will also be a list of
-                                cookware/tools/appliances that they do have access to. Personalizing Kitchen will
-                                allow Kitchen Sync to customize each user’s experience according to their
-                                situation.</p>
+                                can add and remove items in their Pantry, which will tell <strong>Kitchen Sync </strong>
+                                what ingredients you currently have. </p>
 
                             <ButtonToolbar>
-                                <OverlayTrigger trigger="click" placement="bottom" overlay={popoverBottom}>
+                                <OverlayTrigger trigger="click" placement="bottom" overlay={popoverBottom2}>
                                     <button className="btn btn-lg btn-primary">Detail >></button>
                                 </OverlayTrigger>
                             </ButtonToolbar>
@@ -129,14 +157,13 @@ class Homepage extends Component {
 
                         <Col sm={3}>
                             <Image src={browseIcon} circle responsive />
-                            <h2>Planner</h2>
+                            <h2 className="feature-style">Planner</h2>
                             <p> Meal Planner will help users to plan ahead and be ready for meals they plan to
                                 cook beforehand. Users will be able to plan a recipe for a certain date, and Kitchen
-                                Sync
-                                will remind the user to prepare for their cooking expedition. Using information from</p>
+                                Sync will remind the user to prepare for their cooking expedition. </p>
 
                             <ButtonToolbar>
-                                <OverlayTrigger trigger="click" placement="bottom" overlay={popoverBottom}>
+                                <OverlayTrigger trigger="click" placement="bottom" overlay={popoverBottom3}>
                                     <button className="btn btn-lg btn-primary">Detail >></button>
                                 </OverlayTrigger>
                             </ButtonToolbar>
@@ -150,32 +177,27 @@ class Homepage extends Component {
                             <h2 className="featurette-heading">Check out our amazing features. <span
                                 class="text-muted">Kitchen Sync will blow your mind.</span>
                             </h2>
-                            <p className="lead"> Browse recipes from our broad database, Add your own pantries and
-                                ingredients to your Kitchen, Planner reminds you Browse recipes from our broad database, Add your own pantries and
-                                ingredients to your Kitchen, Planner reminds you. Browse recipes from our broad database, Add your own pantries and
-                                ingredients to your Kitchen, Planner reminds you. Browse recipes from our broad database, Add your own pantries and
-                                ingredients to your Kitchen, Planner reminds you..</p>
+                            <p className="lead"> Browse any recipes from our broad recipe database, make your own meal
+                            with your own tools and ingredients! Don't waste your time to find other cooking app, we have
+                            everything you need! </p>
                         </Col>
                         <Col sm={5}>
-                            <Image className="featurette-image img-fluid mx-auto" src={kitchenImage} responsive />
+                            <Image className="featurette-image img-fluid mx-auto" src={lemonImage} responsive />
                         </Col>
                     </Row>
                     <hr class="featurette-divider"/>
                     <Row className="featurette">
                         <div className="col-md-7 order-md-2">
-                            <h2 className="featurette-heading">Oh yeah, it's that good. <span
+                            <h2 className="featurette-heading">Oh yeah, it's time to make your meal. <span
                                 className="text-muted">See for yourself.</span>
                             </h2>
-                            <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id
-                                ligula
-                                porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl
-                                consectetur.
-                                Fusce dapibus, tellus ac cursus commodo.</p>
+                            <p className="lead">Worry about the cost for cooking? Don't worry! Add your own pantries
+                                and ingredients to your My kitchen page and use our amazing filter by cost! Our database
+                                will show you recipes depend on your Kitchen and cost filter!
+                            </p>
                         </div>
                         <div className="col-md-5 order-md-1">
-                            <img class="featurette-image img-fluid mx-auto"
-                                 src="https://www.thelocal.it/userdata/images/article/69523836b0191608c41d640feead8da2be5462038d3409e1e3900fad039c7fc8.jpg"
-                                 alt="Generic placeholder image"/>
+                            <Image className="featurette-image img-fluid mx-auto" src={appleImage} responsive />
                         </div>
                     </Row>
                     <hr class="featurette-divider"/>
@@ -183,16 +205,12 @@ class Homepage extends Component {
                         <div className="col-md-7">
                             <h2 className="featurette-heading">And lastly, this one. <span
                                 className="text-muted">Checkmate.</span></h2>
-                            <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id
-                                ligula
-                                porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl
-                                consectetur.
-                                Fusce dapibus, tellus ac cursus commodo.</p>
+                            <p className="lead">Are you so busy to cook? Don't worry! Use our amazing browse filters!
+                                Filter by time shows you best suitable recipes for you in a second! Best recipe
+                                application for student!</p>
                         </div>
                         <div className="col-md-5">
-                            <img class="featurette-image img-fluid mx-auto"
-                                 src="https://www.thelocal.it/userdata/images/article/69523836b0191608c41d640feead8da2be5462038d3409e1e3900fad039c7fc8.jpg"
-                                 alt="Generic placeholder image"/>
+                            <Image className="featurette-image img-fluid mx-auto" src={limeImage} responsive />
                         </div>
                     </Row>
 
