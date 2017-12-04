87333ad1e79703440471e174f17c480d3619bcc9
johnsonye <johnsonye0303@gmail.com>
2017-12-03 18:28:31 -0800

    Fuck merging

--- recipez-app/src/components/pages/recipe.js
+++ recipez-app/src/components/pages/recipe.js
@@ -1,6 +1,6 @@
 /**
  * Title: Recipe.js
- * Authors: Alexander Haggart & Johnson Ye
+ * Authors: Alexander Haggart
  * Date Created: 11/11/2017
  * Description: This file will load and display a recipe
  */
@@ -14,14 +14,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'react-bootstrap-carousel/dist/react-bootstrap-carousel.css';
 import {Carousel} from 'react-bootstrap';
 
-
 class Recipe extends Component {
     constructor(props){
         super(props)
         this.state = {
             loaded:false,
             data:'Loading . . . ',
-            value:''
+            value:'',
+            active: false
         }
         this.setRecipeData = this.setRecipeData.bind(this);
         this.updateReviews = this.updateReviews.bind(this);
@@ -29,7 +29,8 @@ class Recipe extends Component {
         this.client.loadRecipe(this.props.match.params.recipe,this.setRecipeData,this.props.match.params.user)
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.User = new User();
+        this.reload = this.reload.bind(this);
+        this.User = User.getUser();
     }
 
     setRecipeData(recipeObject,err){
@@ -44,7 +45,8 @@ class Recipe extends Component {
     }
 
     updateReviews(response){
-        this.setState({data:this.state.data})
+        this.client.loadRecipe(this.props.match.params.recipe,this.setRecipeData,this.props.match.params.user)
+        // this.setState({data:this.state.data})
     }
 
     handleChange(event) {
@@ -57,46 +59,50 @@ class Recipe extends Component {
         event.preventDefault();
     }
 
+    reload() {
+        window.location.reload();
+    }
+
     render() {
         if(!this.state.loaded) {
             return <div><h1>{this.state.data}</h1></div>
         }
-
-        /*var ingredients = this.state.data.Ingredients.map((ingredient) => <li class="list-group-item"><span color={this.state.mousedOver === ingredient ? 'red' : 'black'}>{ingredient}</span></li>)*/
-        var ingredients = this.state.data.Ingredients.map((ingredient) =>
-            <li class="list-group-item">
-                <span>
-                    <button onClick={(e)=>this.User.addToShoppingList(ingredient)} type="button" class="btn btn-circle" id="shoppoing_cart">
+        var ingredients = this.state.data.Ingredients.map((ingredient) => <li class="list-group-item">
+                <span className="ingredient-box">
+                    <button onClick={(e)=>this.User.addToShoppingList(ingredient)} className="shopping button" type="button" class="btn btn-circle" id="shoppoing_cart">
                         <i class="glyphicon glyphicon-shopping-cart"/>
-                    </button>
+                    </button>&nbsp;&nbsp;
                     {ingredient}
                 </span>
-            </li>)
+        </li>)
 
-        var directions = this.state.data.Directions.map((step) =>
-            <li class="list-group-item"><i class="fa fa-cutlery" id="forksize"/>{step}</li>)
+        var directions = this.state.data.Directions.map((step) => <li class="list-group-item"><i class="fa fa-cutlery" id="forksize"/>{step}</li>)
 
         // alert(JSON.stringify(this.state.data))
         var reviews = Object.entries(this.state.data.Reviews?this.state.data.Reviews:{}).map((review) =>
             <li>
-                <h4><i class="glyphicon glyphicon-user"/>&nbsp;&nbsp;{review[1].username}
-                <img src={'/star.jpg'} height='21' width='21'/></h4>
-                <p>{review[1].Comment}</p>
+                <div class="panel panel-default">
+                    <div class="panel-heading"><i class="glyphicon glyphicon-user"/>&nbsp;&nbsp;{review[1].username}&nbsp;<img src={'/star.jpg'} height='21' width='21'/></div>
+                    <div class="panel-body"><p>{review[1].Comment}</p></div>
+                </div>
             </li>)
 
-        var updateComment = {username:'USERTESTING',Comment:this.state.value,Rating:'5',timestamp:'-1'}
-
+        var updateComment = {username:this.User.client.getUsername(),Comment:this.state.value,Rating:'5',timestamp:'-1'}
+        var defaultImage1 = "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/ieqr7Lr2x6Ug/v0/800x-1.jpg"
+        var defaultImage2 = "https://s3-ap-northeast-1.amazonaws.com/sharingkyoto2017/articles/KVxqUS8KsRCmG7LTCyM2Tx4xNAdk6s09IKEa5yTU.jpeg"
+        var defaultImage3 = "http://cdn-api.skim.gs/images/view/54be909e3847cf000069016b"
         const carouselInstance = (
-            <Carousel id="Carousel" className="carousel">
-                <Carousel.Item className="carousel-item">
-                    <img src="https://andyloweorg.files.wordpress.com/2017/10/file_5890583196d69functional-foods_1-1024x682.jpg?w=1024&h=576&crop=1" class="img-fluid " alt=""/>
-                </Carousel.Item>
-                <Carousel.Item className="carousel-item">
-                    <img src="http://foodandtravel.mx/home/wp-content/uploads/2017/07/1qTcIHK.jpg" class="img-fluid " alt=""/>
+            <Carousel>
+                <Carousel.Item className="CarouselSize">
+                    <img src={this.state.data.Image?Array.from(this.state.data.Image)[0]:defaultImage1} class="img-fluid"/>
                 </Carousel.Item>
 
-                <Carousel.Item className="carousel-item">
-                    <img src="http://cdn.shopify.com/s/files/1/0164/3912/files/tagliatelle_with_mushrooms_3_1024x1024.jpg?12754498851335701329" class="img-fluid " alt="1024x682"/>
+                <Carousel.Item className="CarouselSize">
+                    <img src={this.state.data.Image?Array.from(this.state.data.Image)[1]:defaultImage2} class="img-fluid "/>
+                </Carousel.Item>
+
+                <Carousel.Item className="CarouselSize">
+                    <img src={this.state.data.Image?Array.from(this.state.data.Image)[2]:defaultImage3} class="img-fluid "/>
                 </Carousel.Item>
             </Carousel>
         );
@@ -104,69 +110,66 @@ class Recipe extends Component {
         return (
             <div>
                 <div className="jumbotron">
-                    <h1>{this.state.data.Name}</h1>
+                    <h1>Kitchen Sync</h1>
                 </div>
+                <h1 className="PageHeader">{this.state.data.Name}</h1>
+                <br/>
+
                 <div class="container">
                     <div class="row">
                         <div class="col-sm-6">
-
-                            {/*<img src={'/Basic Baked Spaghetti.jpg'} class="img-fluid " alt=""/>*/}
+                            {/*show picture*/}
                             <div>
                                 {carouselInstance}
                             </div>
 
-                            <div class="btn-group btn-group-sm">
-                                {/*<button onClick={(e)=>this.client.updateReview(this.state.data.Name,dummyReviewObject,this.updateReviews)} type={"button"} class="btn btn-outline-primary">  UPDATE  </button>*/}
-                                <button onClick={(e)=>User.getUser('user001').getUserData('cookbook').then((data)=>alert(JSON.stringify(data)))} type={"button"} class="btn btn-outline-primary">
-                                    <i class="glyphicon glyphicon-check"/>  check  </button>
-                                <button onClick={(e)=>User.getUser('user001').saveExternalRecipe(this.state.data.Name)} type={"button"} class="btn btn-outline-primary">
-                                    <i class="glyphicon glyphicon-book"/>  add to cookbook  </button>
-                                <button onClick={(e)=>User.getUser('user001').deleteRecipe(this.state.data.Name)} type={"button"} class="btn btn-outline-primary">
-                                    <i class="glyphicon glyphicon-trash"/>  remove from cookbook  </button>
+                            {/*====button group====*/}
+                            <div class="row">
+                                <div class="btn=group btn-group-lg">
+                                    {/*<button onClick={(e)=>this.client.updateReview(this.state.data.Name,dummyReviewObject,this.updateReviews)} type={"button"} class="btn btn-outline-primary">  UPDATE  </button>*/}
+                                    <button onClick={(e)=>User.getUser(this.User.client.getUsername()).getUserData('cookbook').then((data)=>alert(JSON.stringify(data)))} type={"button"} class="btn btn-outline-primary">
+                                        <i class="glyphicon glyphicon-check"/>  check  </button>
+                                    <button onClick={(e)=>User.getUser(this.User.client.getUsername()).saveExternalRecipe(this.state.data.Name)} type={"button"} class="btn btn-outline-primary">
+                                        <i class="glyphicon glyphicon-book"/>  add to cookbook  </button>
+                                    <button onClick={(e)=>User.getUser(this.User.client.getUsername()).deleteRecipe(this.state.data.Name)} type={"button"} class="btn btn-outline-primary">
+                                        <i class="glyphicon glyphicon-trash"/>  remove from cookbook  </button>
+                                </div>
                             </div>
+                            <div class="col">
+                                <h2><i class="glyphicon glyphicon-time"/>&nbsp;&nbsp;{this.state.data.TimeCost}&nbsp;&nbsp;&nbsp;<i class="glyphicon glyphicon-wrench"/>&nbsp;{this.state.data.Difficulty}</h2>
+                            </div>
+                            {/*====button group====*/}
                         </div>
-                        <div class="col-sm-6">
-                            {/*<h1>{this.state.data.Name}</h1>*/}
+                        <div class="col-sm-6" className="Ingredient">
                             <h2>Ingredients:</h2>
                             <ul>{ingredients}</ul>
                         </div>
                     </div>
-                    <h2>Directions:</h2>
+                    <h2 className="Direction">Directions:</h2>
                     <ol>{directions}</ol>
-                    {/*<h2>Reviews:</h2>*/}
-                    {/*<ul>{reviews}</ul>*/}
-                    {/*<h2>JSON:</h2>
-                            <p>{JSON.stringify(this.state.data)}</p>*/}
                 </div>
 
-                <div class="container" id="reviewContainer">
+                <div class="container">
                     <div>
                         {/* Nav bar content here */}
                         <div className="container">
                             <Tabs defaultActiveKey={1}>
                                 <Tab eventKey={1} title={"User Comment"}>
-                                    <div>
-                                        <div class="row">
-                                            User Comment
-                                        </div>
-                                        <div class="row">
-                                            <ul>{reviews}</ul>
-                                        </div>
-                                    </div>
+                                    <ul>{reviews}</ul>
                                 </Tab>
                                 <Tab eventKey={2} title={"Leave a Comment"}>
                                     <form onSubmit={this.handleSubmit}>
-                                        <label>Leave your comment:</label>
-                                        <textarea class="form-control" name="comment" rows="8" id="comment" placeholder="Write a comment here" value={this.state.value} onChange={this.handleChange}/>
+                                        {/*<label>Leave your comment:</label>*/}
+                                        <i class="glyphicon glyphicon-pencil"/>
+                                        <textarea class="form-control" placeholder="Write a comment" name="comment" rows="8" id="comment" value={this.state.value} onChange={this.handleChange}/>
 
                                         <div class="form-group">
                                             <div class="col-12">
                                                 <span className="pull-right">
                                                     <br/>
-                                                    <button onClick={(e)=>this.client.updateReview(this.state.data.Name,updateComment,this.updateReviews)}
-                                                        class="btn btn-success btn-circle text-uppercase" type="submit" id="submitComment">
-                                                        <span class="glyphicon glyphicon-send"/> Submit comment
-                                                    </button>
+                                                    <button onClick={(e)=>{this.client.updateReview(this.state.data.Name,updateComment,this.updateReviews);/*this.reload()*/}}
+                                                            class="btn btn-success btn-circle text-uppercase" type="submit" id="submitComment" >
+                                                        <span class="glyphicon glyphicon-send"/> Summit comment</button>
                                                 </span>
                                             </div>
                                         </div>
@@ -178,9 +181,7 @@ class Recipe extends Component {
                 </div>
 
             </div>
-
-
-            )
+        )
     }
 
 }
